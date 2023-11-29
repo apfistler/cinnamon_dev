62accc183c8dc38d33d74522d833350295baf58c
Adam Fistler <fistlerap@gmail.com>
2023-11-29 22:19:23 +0000

    Add check require fields

--- lib/obj_metadata.py
+++ lib/obj_metadata.py
@@ -7,3 +7,14 @@ class Obj_Metadata(BaseContent):
         super().__init__(obj_metadata_id, obj_metadata_path, data)
         # Add any additional obj_metadata-specific functionality here
 
+        # Check the 'type' field and add 'title' to required fields if type is 'page'
+        if self.type == 'page':
+            self.REQUIRED_FIELDS.append('title')
+
+        self.check_required_fields()
+
+        # Add any additional obj_metadata-specific functionality here
+
+    def check_required_fields(self):
+        super().check_required_fields()  # Call the base class method
+
--- lib/site.py
+++ lib/site.py
@@ -7,3 +7,10 @@ class Site(BaseContent):
         super().__init__(site_id, site_path, data)
         # Add any additional site-specific functionality here
 
+        self.check_required_fields()
+
+        # Add any additional obj_metadata-specific functionality here
+
+    def check_required_fields(self):
+        super().check_required_fields()  # Call the base class method
+
